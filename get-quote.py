6abc904b38dbca8ae7d847c840a8c60cfbac8018
trhiana <trhiana@rocketmail.com>
2021-03-15 18:54:55 +0300

    Random quote bot is running

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
+
 def primary():
-  print("Keep it logically awesome.")
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  length = len(quotes) - 1
+  randomQuote = random.randint(0, length)
+  print(quotes[randomQuote])
 
 if __name__== "__main__":
   primary()
